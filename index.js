9b7d198fc40eb12beb08af0ddc19477131df71b4
Subhan Baloch <160255991+subhanbaloch18@users.noreply.github.com>
2024-04-18 03:17:52 -0700

    Updated Words Counter App!!!

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Write your sentence to count the written words"
+    }]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`The total counted sentence words are ${words.length}`);
+console.log("Thank you for Using s2bl words counter app!!!");
