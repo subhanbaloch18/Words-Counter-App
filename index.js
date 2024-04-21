eb6a7ae7d26d5911eaf08e133d6a4d758e691340
Subhan Baloch <160255991+subhanbaloch18@users.noreply.github.com>
2024-04-21 01:38:06 -0700

    Done some changings in the index.ts file !!!

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answers = await inquirer.prompt([{
         name: "Sentence",
         type: "input",
-        message: "Write your sentence to count the written words"
+        message: `${chalk.dim.italic("Write your sentence to count the written words")}`
     }]);
 const words = answers.Sentence.trim().split(" ");
 console.log(words);
-console.log(`The total counted sentence words are ${words.length}`);
-console.log("Thank you for Using s2bl words counter app!!!");
+console.log `${chalk.yellow.bold.underline.italic `The total counted sentence words are ${words.length}`}`;
+console.log `${chalk.blueBright.bold `Thank you for Using s2bl words counter app!!!`}`;
--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 
 import inquirer from "inquirer";
 
+import chalk from "chalk";
+
 const answers : {
 
 Sentence : "string"
@@ -10,16 +12,16 @@ Sentence : "string"
 
     name : "Sentence",
     type : "input",
-    message : "Write your sentence to count the written words"
+    message : `${chalk.dim.italic("Write your sentence to count the written words" )}`
 }])
 
 const words = answers.Sentence.trim().split(" ");
 
 console.log(words);
 
-console.log(`The total counted sentence words are ${words.length}`);
+console.log`${chalk.yellow.bold.underline.italic`The total counted sentence words are ${words.length}`}`;
 
-console.log("Thank you for Using s2bl words counter app!!!");
+console.log`${chalk.blueBright.bold`Thank you for Using s2bl words counter app!!!`}`;
 
 
 
